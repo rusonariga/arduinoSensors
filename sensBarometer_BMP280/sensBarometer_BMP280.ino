5057df9f9536e512199d7c03579e2102284e536a
rusonariga <50117587+rusonariga@users.noreply.github.com>
2021-12-09 14:22:11 -0300

    Barometer BMP280

---
+++ sensBarometer_BMP280/sensBarometer_BMP280.ino
@@ -0,0 +1,138 @@
+
+#include <Adafruit_BMP280.h>
+#include <LiquidCrystal_I2C.h>
+//#include <SPI.h>
+#include <Wire.h>
+
+// You will need to create an SFE_BMP180 object, here called "pressure":
+SFE_BMP180 pressure;
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+//#define BMP_SCK  (13)
+//#define BMP_MISO (12)
+//#define BMP_MOSI (11)
+//#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+
+
+void setup()
+{
+  Serial.begin(9600); //initialize the serial monitor
+  Serial.println("REBOOT");//ptint "REBOOT"
+
+  // Initialize the sensor (it is important to get calibration values stored on the device).
+  if (pressure.begin())
+    Serial.println("BMP180 init success");
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    Serial.println("BMP180 init fail\n\n");
+    while(1); // Pause forever.
+  }
+  lcd.init();  //initialize the lcd
+  lcd.backlight();  //open the backlight 
+}
+
+void loop()
+{
+  char status;
+  double T,P,p0,a;
+
+  // Loop here getting pressure readings every 10 seconds.
+
+  // If you want sea-level-compensated pressure, as used in weather reports,
+  // you will need to know the altitude at which your measurements are taken.
+  // We're using a constant called ALTITUDE in this sketch:
+
+  // If you want to measure altitude, and not pressure, you will instead need
+  // to provide a known baseline pressure. This is shown at the end of the sketch.
+  // You must first get a temperature measurement to perform a pressure reading.
+
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.println(" deg C ");
+      float tempF = 1.8*T + 32.0;       // convert °C to °F
+      Serial.print("             ");
+      Serial.print(tempF,2);
+      Serial.println(" deg F ");
+      Serial.println(" "); // skip a line
+
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("pressure: ");
+          Serial.print(P,2);
+          Serial.println(" mbar ");
+          Serial.print("          ");
+          Serial.print(P*0.0295301,2); //To convert millibars to inches of mercury
+          Serial.print(" inches Hg ");
+          Serial.println(" "); //
+          Serial.print("          ");
+          Serial.print(P*100,2); //To convert Pascal
+          Serial.print(" Pa ");          
+          Serial.println(" "); // skipping 2 lines for easier reading
+          Serial.println(" "); //
+          // The pressure sensor returns abolute pressure, which varies with altitude.
+          // To remove the effects of altitude, use the sealevel function and your current altitude.
+          // This number is commonly used in weather reports.
+          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+          // Result: p0 = sea-level compensated pressure in mb
+
+          // LCD DISPLAY DATA
+            lcd.setCursor(0, 0);
+            lcd.print("Pre   Temp");
+            lcd.setCursor(0, 1);
+            lcd.print(P,2); //print the temperature on lcd
+            lcd.setCursor(6, 1);
+            lcd.print(T,2);
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
+    }
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+  delay(1000);  // Pause for 5 seconds.
+}
